74541f1f109adf9d1cceee615e24952fcdb33600
Mathias Spurr <scitus@hotmail.dk>
2015-09-03 09:05:44 +0200

    Added chatrooms create update destroy ability

--- Gemfile
+++ Gemfile
@@ -9,7 +9,10 @@ gem 'jquery-rails'
 gem 'turbolinks'
 gem 'jbuilder', '~> 2.0'
 gem 'sdoc', '~> 0.4.0', group: :doc
-
+gem 'haml', '~> 4.0.7'
+gem 'simple_form', '~> 3.1.1'
+gem 'devise', '~> 3.5.2'
+gem 'paperclip', '~> 4.3.0'
 
 
 group :development, :test do
--- app/controllers/chatrooms_controller.rb
+++ app/controllers/chatrooms_controller.rb
@@ -1,6 +1,50 @@
 class ChatroomsController < ApplicationController
+	before_action :find_room, only: [:show, :edit, :update, :destroy]
+
 	def index
+		@room = Chatroom.all.order('created_at DESC')
 	end
 
-	
+	def show
+	end
+
+	def new
+		@room = Chatroom.new
+	end
+
+	def create
+		@room = Chatroom.new(room_params)
+
+		if @room.save
+			redirect_to @room
+		else
+			render 'new'
+		end
+	end
+
+	def edit
+	end
+
+	def update
+		if @room.update(room_params)
+			redirect_to @room
+		else
+			render 'edit'
+		end
+	end
+
+	def destroy
+		@room.destroy
+		redirect_to root_path
+	end
+
+	private
+
+	def find_room
+		@room = Chatroom.find(params[:id])
+	end
+
+	def room_params
+		params.require(:chatroom).permit(:title, :description)
+	end
 end
