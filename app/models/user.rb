f1e005e729d57b50e6f1d43d94ed71bee98b4147
Mathias Spurr <scitus@hotmail.dk>
2015-09-03 10:34:12 +0200

    Added username with uniqueness and relation to chatrooms

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
-
+	validates_uniqueness_of :username
   	devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-         
+
 	has_many :chatrooms
 end
