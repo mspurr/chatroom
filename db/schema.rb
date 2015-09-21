2ae9d71473c902437225c529298dfc7c013ad68f
Mathias Spurr <scitus@hotmail.dk>
2015-09-22 00:43:44 +0200

    Added pg and database

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150914181856) do
+ActiveRecord::Schema.define(version: 20150914115520) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
 
   create_table "broadcasts", force: :cascade do |t|
     t.text     "content"
@@ -25,8 +28,8 @@ ActiveRecord::Schema.define(version: 20150914181856) do
     t.datetime "image_updated_at"
   end
 
-  add_index "broadcasts", ["chatroom_id"], name: "index_broadcasts_on_chatroom_id"
-  add_index "broadcasts", ["user_id"], name: "index_broadcasts_on_user_id"
+  add_index "broadcasts", ["chatroom_id"], name: "index_broadcasts_on_chatroom_id", using: :btree
+  add_index "broadcasts", ["user_id"], name: "index_broadcasts_on_user_id", using: :btree
 
   create_table "chatrooms", force: :cascade do |t|
     t.string   "title"
@@ -35,17 +38,6 @@ ActiveRecord::Schema.define(version: 20150914181856) do
     t.datetime "updated_at",  null: false
     t.integer  "user_id"
   end
-
-  create_table "comments", force: :cascade do |t|
-    t.text     "content"
-    t.integer  "broadcast_id"
-    t.integer  "user_id"
-    t.datetime "created_at",   null: false
-    t.datetime "updated_at",   null: false
-  end
-
-  add_index "comments", ["broadcast_id"], name: "index_comments_on_broadcast_id"
-  add_index "comments", ["user_id"], name: "index_comments_on_user_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -63,8 +55,10 @@ ActiveRecord::Schema.define(version: 20150914181856) do
     t.string   "username"
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-  add_index "users", ["username"], name: "index_users_on_username", unique: true
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
+  add_index "users", ["username"], name: "index_users_on_username", unique: true, using: :btree
 
+  add_foreign_key "broadcasts", "chatrooms"
+  add_foreign_key "broadcasts", "users"
 end
