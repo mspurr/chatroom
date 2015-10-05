6a3c8400a874fa673f50adf07e200c9096b68ef9
Taylor E Kems <taylor.kems@generalassemb.ly>
2015-10-05 15:55:28 -0500

    rebase conflicts

--- app/controllers/chatrooms_controller.rb
+++ app/controllers/chatrooms_controller.rb
@@ -1,5 +1,4 @@
 class ChatroomsController < ApplicationController
-<<<<<<< HEAD
   before_action :authenticate_user!, except: [:index]
   before_action :find_room, only: [:show, :edit, :update, :destroy]
   before_action :random_room, only: [:show, :index]
@@ -59,67 +58,4 @@ class ChatroomsController < ApplicationController
   def room_params
     params.require(:chatroom).permit(:title, :description, :game_name)
   end
-=======
-	before_action :find_room, only: [:show, :edit, :update, :destroy]
-	before_action :random_room, only: [:show, :index]
-	before_action :authenticate_user!, except: [:index]
-
-	def index
-		if params[:game].blank?
-			@room = Chatroom.all.order("created_at DESC")
-		else
-			@game_id = Game.find_by(name: params[:game]).id
-			@room = Chatroom.where(game_id: @game_id).order("created_at DESC")
-		end
-	end
-
-	def show
-		@broadcast = @room.broadcasts.all.order('created_at DESC')
-	end
-
-	def new
-		@room = current_user.chatrooms.build
-	end
-
-	def create
-		@room = current_user.chatrooms.build(room_params)
-
-		if @room.save
-			redirect_to @room
-		else
-			flash[:alert] = "Game not found. Please select a game from the list."
-			render 'new'
-		end
-	end
-
-	def edit
-	end
-
-	def update
-		if @room.update(room_params)
-			redirect_to @room
-		else
-			render 'edit'
-		end
-	end
-
-	def destroy
-		@room.dest
-		redirect_to root_path
-	end
-
-	private
-
-	def find_room
-		@room = Chatroom.find(params[:id])
-	end
-
-	def random_room
-		@random = Chatroom.all.limit(5).shuffle
-	end
-
-	def room_params
-		params.require(:chatroom).permit(:title, :description, :game_name)
-	end
->>>>>>> Browse Games page - simple
 end
